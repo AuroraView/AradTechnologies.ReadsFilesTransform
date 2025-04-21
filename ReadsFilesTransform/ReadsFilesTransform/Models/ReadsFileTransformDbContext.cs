0946b884f11594ea9122e39e29a58e81985490f2
Shoval Kadosh <shovalkadosh@gmail.com>
2025-04-21 17:25:08 +0300

    PR Changes

--- ReadsFilesTransform/ReadsFilesTransform/Models/ReadsFileTransformDbContext.cs
+++ ReadsFilesTransform/ReadsFilesTransform/Models/ReadsFileTransformDbContext.cs
@@ -9,8 +9,24 @@ namespace ReadsFilesTransform
     /// <seealso cref="Microsoft.EntityFrameworkCore.DbContext" />
     public class ReadsFileTransformDbContext : DbContext
     {
+        /// <summary>Initializes a new instance of the <see cref="ReadsFileTransformDbContext" /> class.</summary>
+        /// <param name="options">The options.</param>
         public ReadsFileTransformDbContext(DbContextOptions<ReadsFileTransformDbContext> options) : base(options) { }
+
+        /// <summary>
+        /// Gets or sets the properties.
+        /// </summary>
+        /// <value>
+        /// The properties.
+        /// </value>
         public DbSet<Properties> Properties { get; set; }
+
+        /// <summary>
+        /// Gets or sets the reads file transform audit.
+        /// </summary>
+        /// <value>
+        /// The reads file transform audit.
+        /// </value>
         public DbSet<ReadsFileTransformAudit> ReadsFileTransformAudit { get; set; }
     }
 }
