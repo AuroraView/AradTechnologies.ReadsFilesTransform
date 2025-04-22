1e5d60320610503e289da95497edb71e87151d00
Moshe Levy <moshel@auroraview.com>
2025-04-22 21:41:01 +0300

    handle duplicate processing - files with the same name that were already processed will move to Error file  and will not be re-processed

--- ReadsFilesTransform/ReadsFilesTransform/ReadsFileProcessor.cs
+++ ReadsFilesTransform/ReadsFilesTransform/ReadsFileProcessor.cs
@@ -97,8 +97,8 @@ namespace ReadsFilesTransform
             _logger.Info($"New file detected: {e.Name}");
             try
             {
-                // Ensure input file exists, accessible, Not empty and process it
-                if (File.Exists(e.FullPath) && !IsEmptyInputFile(e))
+                // Ensure input file exists, accessible, Not empty ,not duplicate (processed in past) and process it
+                if (File.Exists(e.FullPath) && !IsEmptyInputFile(e) && !IsDuplicateInputFile(e))
                 {
                     //in case of single file - open DB connection and read mapping table.
                     if (!_isMultiFilesMode) GetMappingFromDb();
@@ -396,12 +396,40 @@ namespace ReadsFilesTransform
             if (new FileInfo(e.FullPath).Length == 0)
             {
                 _logger.Info($"File is Empty, Skipping processing.");
-                // Move file to Archive 
+                // Move file to Archive or to error if duplicate
                 _fullArchivePath = Path.Combine(_mekorotArchivePath, e.Name);
                 if (!File.Exists(_fullArchivePath))
                 {
                     _logger.Info($"File moved to: {_fullArchivePath}");
                     File.Move(e.FullPath, _fullArchivePath);
+                }
+                else
+                {
+                    _fullErrortPath = GetTargetFileName(e.Name, _mekorotErrPath);
+                    _logger.Info($"File moved to: {_fullErrortPath}");
+                    Thread.Sleep(500);
+                    File.Move(e.FullPath, _fullErrortPath);
+                }
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Determines whether [is duplicate input file] .
+        /// </summary>
+        private bool IsDuplicateInputFile(FileSystemEventArgs e)
+        {
+            _fullArchivePath = Path.Combine(_mekorotArchivePath, e.Name);
+            if (File.Exists(_fullArchivePath))
+            {
+                _logger.Info($"File is duplicate, moved error files and Skip processing.");
+                _fullErrortPath = GetTargetFileName(e.Name, _mekorotErrPath);
+
+                if (!File.Exists(_fullErrortPath))
+                {
+                    _logger.Info($"File moved to: {_fullErrortPath}");
+                    Thread.Sleep(500);
+                    File.Move(e.FullPath, _fullErrortPath);
                 }
                 return true;
             }
